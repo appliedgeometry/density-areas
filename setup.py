ef2e13ada303963b816059c1389d4b977c4b9200
J. C. Ruíz-Pantaleón <49538929+jcrpanta@users.noreply.github.com>
2021-03-19 20:40:37 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+  name='',
+  version='',
+  author="",
+  author_email="",
+  license="MIT",
+  description="",
+  url="",
+  packages=setuptools.find_packages(),
+  install_requires=['numexp'],
+  classifiers=[
+    "Development Status :: 4 - Beta",
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
